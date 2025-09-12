543deb2793d4e79c4491192e9bb969110905b5ea
linyu3294 <yu.lin@later.com>
2025-09-12 01:37:06 -0400

    Delete empty lines of code

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -229,8 +229,6 @@ resource "aws_iam_role_policy_attachment" "ec2_session_manager_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-
-
 resource "aws_iam_instance_profile" "ec2_session_manager_profile" {
   name = "${var.project_name}-ec2-session-manager-profile"
   role = aws_iam_role.ec2_session_manager_role.name
