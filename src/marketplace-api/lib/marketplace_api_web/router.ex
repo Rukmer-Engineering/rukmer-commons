983274dffc519c6f68210b06c887fbe2b97a072d
linyu3294 <yu.lin@later.com>
2025-10-05 23:55:23 -0400

    Add back health check endpoint; Note: this one is restful in contrast to the websocket counter part

--- src/marketplace-api/lib/marketplace_api_web/router.ex
+++ src/marketplace-api/lib/marketplace_api_web/router.ex
@@ -1,6 +1,7 @@
 defmodule MarketplaceApiWeb.Router do
   use Phoenix.Router
   import Phoenix.LiveView.Router
+  import Plug.Conn
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -10,6 +11,8 @@ defmodule MarketplaceApiWeb.Router do
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
+
+  get "/health", do: send_resp(conn, 200, "ok")
 
   scope "/", MarketplaceApiWeb do
     pipe_through :browser
