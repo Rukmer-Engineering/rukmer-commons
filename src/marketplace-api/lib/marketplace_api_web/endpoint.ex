5acb0821edeab54bbc718fffad0a68623840c3cc
linyu3294 <yu.lin@later.com>
2025-10-05 20:44:42 -0400

    Setup basic Phoenix application framework

---
+++ src/marketplace-api/lib/marketplace_api_web/endpoint.ex
@@ -0,0 +1,39 @@
+defmodule MarketplaceApiWeb.Endpoint do
+  use Phoenix.Endpoint, otp_app: :marketplace_api
+
+  @session_options [
+    store: :cookie,
+    key: "_marketplace_api_key",
+    signing_salt: Application.compile_env(:phoenix_live_view, :signing_salt)
+  ]
+
+  socket "/live", Phoenix.LiveView.Socket,
+    websocket: [connect_info: [session: @session_options]]
+
+  # Serve static files and handle errors
+  plug Plug.Static,
+    at: "/",
+    from: :marketplace_api,
+    gzip: false,
+    only: ~w(assets fonts images favicon.ico robots.txt)
+
+  # Code reloading in development
+  if code_reloading? do
+    socket "/phoenix/live_reload/socket", Phoenix.LiveReload.Socket
+    plug Phoenix.LiveReload
+    plug Phoenix.CodeReloader
+  end
+
+  plug Plug.RequestId
+  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+
+  plug Plug.Parsers,
+    parsers: [:urlencoded, :multipart, :json],
+    pass: ["*/*"],
+    json_decoder: Phoenix.json_library()
+
+  plug Plug.MethodOverride
+  plug Plug.Head
+  plug Plug.Session, @session_options
+  plug MarketplaceApiWeb.Router
+end
